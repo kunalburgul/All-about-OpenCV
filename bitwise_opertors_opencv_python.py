452b1239c67945c560cc54b4c7974d68c25a1535
kunalburgul <kunal.burgul@gmail.com>
2020-04-15 21:56:56 +0530

    updated

--- bitwise_opertors_opencv_python.py
+++ bitwise_opertors_opencv_python.py
@@ -5,6 +5,7 @@ img1 = np.zeros((250, 500, 3),np.uint8)
 img1 = cv2.rectangle(img1, (200, 0), (300, 100), (255, 255, 255), -1)
 img2 = cv2.imread("image_1.png")
 
+
 #And
 bitAnd = cv2.bitwise_and(img2, img1)
 
